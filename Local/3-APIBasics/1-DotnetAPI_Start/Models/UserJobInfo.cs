212835b44073508eb33052547f644e8231b94e8a
Abiola <ajibolaabiolaahmed@gmail.com>
2025-06-11 13:35:02 +0100

    Override Tostring methods to return JSON

--- Local/3-APIBasics/1-DotnetAPI_Start/Models/UserJobInfo.cs
+++ Local/3-APIBasics/1-DotnetAPI_Start/Models/UserJobInfo.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 namespace DotnetAPI.Models
 {
     public partial class UserJobInfo
@@ -5,6 +7,11 @@ namespace DotnetAPI.Models
         public int UserId { get; set; }
         public string JobTitle { get; set; } = "";
         public string Department { get; set; } = "";
+
+        override public string ToString()
+        {
+            return JsonSerializer.Serialize(new { UserId, JobTitle, Department });
+        }
 
     }
 }
--- Local/3-APIBasics/1-DotnetAPI_Start/Models/UserSalary.cs
+++ Local/3-APIBasics/1-DotnetAPI_Start/Models/UserSalary.cs
@@ -1,8 +1,15 @@
+using System.Text.Json;
+
 namespace DotnetAPI.Models
 {
     public partial class UserSalary
     {
-        public int UserId {get; set;}
-        public decimal Salary {get; set;} 
+        public int UserId { get; set; }
+        public decimal Salary { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(new { UserId, Salary });
+        }
     }
 }
