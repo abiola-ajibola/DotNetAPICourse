4e6870ed76cf5ecaf7b1d835ab0f66cd661921e2
Abiola <ajibolaabiolaahmed@gmail.com>
2025-06-16 04:25:04 +0100

    Async Await

---
+++ Local/8-HelloWorld_AsyncAwait/Program.cs
@@ -0,0 +1,43 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/
+// https://learn.microsoft.com/en-us/dotnet/csharp/asynchronous-programming/async-scenarios
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Task secondTask = ConsoleAfterDelayAsync("Task 2", 150);
+            Task thirdTask = ConsoleAfterDelayAsync("Task 3", 50);
+            Task firstTask = new Task(() =>
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Task 1");
+
+            });
+            firstTask.Start();
+
+
+            ConsoleAfterDelay("Delay", 75);
+
+
+            Console.WriteLine("After the Task was created");
+            await firstTask;
+            await secondTask;
+            await thirdTask;
+        }
+
+        static void ConsoleAfterDelay(string text, int delayTime)
+        {
+            Thread.Sleep(delayTime);
+            Console.WriteLine(text);
+        }
+
+        static async Task ConsoleAfterDelayAsync(string text, int delayTime)
+        {
+            await Task.Delay(delayTime);
+            Console.WriteLine(text);
+        }
+
+    }
+}
